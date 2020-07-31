9d9815f007f241a4b9dfd146aa103d2caccac7bc
Gian Michel <gianmnf@unipam.edu.br>
2020-07-31 13:20:58 -0300

    Aula 04 - AUTOMATIZANDO COM ARTISAN E RESOURCE

---
+++ ep04/resources/views/editUser.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Editar Usuário</title>
+</head>
+<body>
+
+    <form action="{{ route('user.update', ['user' => $user->id]) }}" method="post">
+        @csrf
+        @method('put')
+        <label for="">Nome do Usuário:</label>
+        <input type="text" name="name" value="{{ $user->name }}">
+
+        <label for="">E-mail do Usuário:</label>
+        <input type="email" name="email" value="{{ $user->email }}">
+
+        <label for="">Senha do Usuário:</label>
+        <input type="password" name="password">
+
+        <input type="submit" value="Editar Usuário">
+    </form>
+
+</body>
+</html>
---
+++ ep04/resources/views/listAllUsers.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Listagem de Usuários</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td>#ID</td>
+        <td>Nome:</td>
+        <td>E-mail:</td>
+        <td>Ações:</td>
+    </tr>
+    @foreach($users as $user)
+    <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->email }}</td>
+        <td>
+        <a href="{{ route('user.show', ['user' => $user->id])}}">Ver Usuário</a>
+        <form action="{{ route('user.destroy', ['user' => $user->id])}}" method="post">
+                @csrf
+                @method('delete')
+                <input type="submit" value="Remover">
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+</body>
+</html>
